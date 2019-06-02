e1d4e934e44c3d432e64d9a42881a77a1c3d98e3
Burak Tokak <iletisim@buraktokak.com>
2019-06-03 00:59:52 +0300

    revert old toolbar

--- src/components/Toolbar/index.js
+++ src/components/Toolbar/index.js
@@ -16,8 +16,8 @@ class App extends Component {
   componentDidMount(){
     Event.on("toggle", (tab) => {
       if(tab !== false){
-        if(this.state.currentTab !== tab){
-          if(this.state.currentTab !== false){
+        if(this.state.currentTab != tab){
+          if(this.state.currentTab != false){
             this.setState({
               tabTransition: 0
             });
@@ -78,39 +78,42 @@ class App extends Component {
   render() {
     return (
       <>
-        <div className="ToolbarPlaceholder"></div>
-        <div className="Toolbar" style={{width: this.state.transition ? 400 : 50}}>
+        <div className="Toolbar" style={{width: this.state.transition == 1 ? 400 : 50}}>
           <div className="Menu">
             <div className="Top">
-              <div className="Item" onClick={() => Event.emit("toggle", "sheets")} style={{backgroundColor: this.state.currentTab == "sheets" ? "transparent" : "transparent"}}>
+              <div className="Item Search" onClick={() => Event.emit("toggle", "search")} style={{backgroundColor: this.state.currentTab == "search" ? "#f1f1f1" : "transparent"}}>
+                <div className="ToolTip"><span>Search</span></div>
+                <img src={require("../../icon/icon-search.svg")}/>
+              </div>
+              <div className="Item" onClick={() => Event.emit("toggle", "sheets")} style={{backgroundColor: this.state.currentTab == "sheets" ? "#f1f1f1" : "#ffffff"}}>
                 <div className="ToolTip"><span>Sheets</span></div>
                 <img src={require("../../icon/icon-duplicate.svg")}/>
               </div>
-              <div className="Item" onClick={() => Event.emit("toggle", "archives")} style={{backgroundColor: this.state.currentTab == "archives" ? "transparent" : "transparent"}}>
+              <div className="Item" onClick={() => Event.emit("toggle", "archives")} style={{backgroundColor: this.state.currentTab == "archives" ? "#f1f1f1" : "#ffffff"}}>
                 <div className="ToolTip"><span>Archives</span></div>
                 <img src={require("../../icon/icon-archive.svg")}/>
               </div>
-              <div className="Item" onClick={() => Event.emit("toggle", "attachments")} style={{backgroundColor: this.state.currentTab == "attachments" ? "transparent" : "transparent"}}>
+              <div className="Item" onClick={() => Event.emit("toggle", "attachments")} style={{backgroundColor: this.state.currentTab == "attachments" ? "#f1f1f1" : "#ffffff"}}>
                 <div className="ToolTip"><span>Attachments</span></div>
                 <img src={require("../../icon/icon-clip.svg")}/>
               </div>
             </div>
             <div className="Bottom">
-              <div className="Item" onClick={() => Event.emit("toggle", "addons")} style={{backgroundColor: this.state.currentTab == "addons" ? "transparent" : "transparent"}}>
+              <div className="Item" onClick={() => Event.emit("toggle", "addons")} style={{backgroundColor: this.state.currentTab == "addons" ? "#f1f1f1" : "#ffffff"}}>
                 <div className="ToolTip"><span>Addons</span></div>
                 <img src={require("../../icon/icon-puzzle.svg")}/>
               </div>
-              <div className="Item" onClick={() => Event.emit("toggle", "settings")} style={{backgroundColor: this.state.currentTab == "settings" ? "transparent" : "transparent"}}>
+              <div className="Item" onClick={() => Event.emit("toggle", "settings")} style={{backgroundColor: this.state.currentTab == "settings" ? "#f1f1f1" : "#ffffff"}}>
                 <div className="ToolTip"><span>Settings</span></div>
                 <img src={require("../../icon/icon-cog.svg")}/>
               </div>
             </div>
           </div>
-          <div className="tabContent">
+          <div className="tabContent" style={{opacity: this.state.tabTransition, left: this.state.tabTransition == 1 ? 0 : 10}}>
             {this.renderTabContent(this.state.currentTab)}
           </div>
         </div>
-        {false &&
+        {this.state.currentTab &&
           <div
             className="shadow"
             style={{opacity: this.state.transition}}
