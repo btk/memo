89846ebe19a0296ff0f0c37acc007755f0b6fbff
Burak Tokak <iletisim@buraktokak.com>
2019-12-21 16:22:28 +0300

    betrter push button

--- src/tabs/Push/index.js
+++ src/tabs/Push/index.js
@@ -6,7 +6,7 @@ import Loading from '../../components/Loading';
 class App extends Component {
 
   state = {
-    syncText: API.getData("staging") ? "Push" : "Synced",
+    syncText: API.getData("staging") ? "Push All Changes" : "No Local Changes",
     stagedAmount: API.getData("staging") ? API.getData("staging").split(",").length : 0
   }
 
@@ -20,9 +20,9 @@ class App extends Component {
 
   syncAction = (status) => {
     if(status == "flushed"){
-      this.setState({syncText: "Synced"});
+      this.setState({syncText: "No Local Changes"});
     }else{
-      this.setState({syncText: "Push", stagedAmount: status});
+      this.setState({syncText: "Push All Changes", stagedAmount: status});
     }
   }
 
@@ -85,8 +85,16 @@ class App extends Component {
         <h4>Push Changes</h4>
         <p className="sub">You can push changes to your Gist and sync your data.</p>
 
-        <div className="syncButton" onClick={() => this.startSync()}>
-          {this.state.syncText} {this.renderStagedAmount(this.state.stagedAmount)}
+        <div className="tabScroller">
+          <div
+            className="sheetItem addNew"
+            key={"push_changes"}
+            onClick={() => this.startSync()}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="#444" viewBox="0 0 24 24" width="18" height="18">
+                <path d="M13 5.41V17a1 1 0 0 1-2 0V5.41l-3.3 3.3a1 1 0 0 1-1.4-1.42l5-5a1 1 0 0 1 1.4 0l5 5a1 1 0 1 1-1.4 1.42L13 5.4zM3 17a1 1 0 0 1 2 0v3h14v-3a1 1 0 0 1 2 0v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-3z"/>
+              </svg>
+              <span>{this.state.syncText} {this.renderStagedAmount(this.state.stagedAmount)}</span>
+          </div>
         </div>
       </div>
     );
